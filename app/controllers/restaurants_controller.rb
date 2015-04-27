ea08866553564e063dff678940c9dc3cc44f9c48
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-04-27 00:14:50 -0400

    Generated Restaurant model, defined actions in controller and filled erb in corresponding views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,34 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+     redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name,:address,:website_url)
+  end
+
+end
