7fd643dc438a17695575507bc5612ab01a34c6cd
Ezaam Yusuf <ezaamyusuf@gmail.com>
2015-07-08 16:53:55 -0400

    styled signup page;fixed nav restaurant link from user page;modified sessions controller to stay on same page after logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to restaurants_path, notice: "Logged out!"
+    redirect_to :back, notice: "Logged out!"
 
   end
 end
